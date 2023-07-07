0ea0fc5ba0c637f1206c5c1a73dbae309eb1212d
roshanleharwani <roshanleharwani@gmail.com>
2023-07-07 19:36:29 +0530

    threading process icon remaing else done

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import ttkbootstrap as ttk
 from PIL import Image, ImageTk
 import customtkinter as ctk
-from submit_handler import respone_genrator
+
+import openai
 
 
 def image_parser(path: str, size: tuple):
@@ -22,7 +23,7 @@ class App(ttk.Window):
         self.label = ttk.Label(self, text="Query", font=("Helvetica", 15, "italic"))
         self.label.place(relx=0.5, rely=0.43, anchor="center")
         self.query = ttk.Entry(
-            self, bootstyle="success", textvariable=self.query_var, width=50
+            self, bootstyle="success", textvariable=self.query_var, width=75
         )
         self.query.place(relx=0.5, rely=self.y, anchor="center")
         self.widget_creator()
@@ -43,14 +44,27 @@ class App(ttk.Window):
             hover_color="#002B36",
             command=lambda: (self.query_var.set("")),
         )
-        self.result_box = ctk.CTkTextbox(self, height=375, width=500)
+        self.result_box = ctk.CTkTextbox(
+            self, height=375, width=500, font=("Helvetica", 16, "normal")
+        )
+        self.reset_btn = ttk.Button(
+            self,
+            text="Reset",
+            command=self.reset,
+            bootstyle="success-outline",
+        )
+        self.quit_btn = ttk.Button(
+            self,
+            text="Quit",
+            bootstyle="danger-outline",
+            command=self.destroy,
+        )
         self.mainloop()
 
     def widget_creator(self):
         def creator(_):
             self.sub_btn.place(relx=0.5, rely=0.6, anchor="center")
             self.clear_btn.place(relx=0.775, rely=0.47)
-            # self.query.bind("<Return>", lambda _: self.animate_widget(_))
 
         self.query.bind("<KeyRelease>", lambda _: creator(_))
 
@@ -68,13 +82,33 @@ class App(ttk.Window):
                 self.query.configure(state="readonly")
                 self.result_box.pack(pady=5, padx=5, expand=True)
                 self.inserter()
+                self.reset_btn.place(relx=0.5, rely=0.85, anchor="center")
+                self.quit_btn.place(relx=0.6, rely=0.85, anchor="center")
 
         animate()
 
     def inserter(self):
         query = str(self.query_var.get())
-        result = respone_genrator(query=query)
+        result = self.respone_genrator(query)
         self.result_box.insert("end", result)
+
+    def reset(self):
+        self.result_box.pack_forget()
+        self.query.place_forget()
+        self.y = 0.5
+        self.query.place(relx=0.5, rely=self.y, anchor="center")
+        self.query.configure(state="normal")
+        self.query_var.set("")
+        self.label.place(relx=0.5, rely=0.43, anchor="center")
+        self.reset_btn.place_forget()
+        self.quit_btn.place_forget()
+
+    def respone_genrator(self, query: str) -> str:
+        openai.api_key = open("api.txt", "r").read()
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}]
+        )
+        return str((dict(((response["choices"])[0])["message"]))["content"])
 
 
 if __name__ == "__main__":
--- submit_handler.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-
-def respone_genrator(query: str) -> str:
-    openai.api_key = open("api.txt", "r").read()
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}]
-    )
-    return str((dict(((response["choices"])[0])["message"]))["content"])
